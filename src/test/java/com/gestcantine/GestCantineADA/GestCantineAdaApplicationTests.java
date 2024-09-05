7cbae4ec76f52c63c7e79e34002ec07bc8d64b68
Bino26 <hbino1999@gmail.com>
2024-09-05 14:30:13 +0000

    feat: init project

---
+++ src/test/java/com/gestcantine/GestCantineADA/GestCantineAdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestcantine.GestCantineADA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestCantineAdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
