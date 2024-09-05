a7d2e7c0da0e16287ca267b8308581e9d2ba77f2
Bino26 <hbino1999@gmail.com>
2024-09-05 18:23:53 +0000

    fix: Fix PlatMapper

--- src/main/java/com/gestcantine/GestCantineADA/mapping/implementations/PlatMapper.java
+++ src/main/java/com/gestcantine/GestCantineADA/mapping/implementations/PlatMapper.java
@@ -1,18 +1,24 @@
 package com.gestcantine.GestCantineADA.mapping.implementations;
 
+import com.gestcantine.GestCantineADA.dtos.MenuDTO;
 import com.gestcantine.GestCantineADA.dtos.PlatDTO;
 import com.gestcantine.GestCantineADA.mapping.contracts.IPlatMapper;
+import com.gestcantine.GestCantineADA.models.Menu;
 import com.gestcantine.GestCantineADA.models.Plat;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PlatMapper implements IPlatMapper {
+    private ModelMapper modelMapper ;
 
     @Override
     public PlatDTO toDto(Plat entity) {
-        return null;
+        return modelMapper.map(entity, PlatDTO.class);
     }
 
     @Override
     public Plat toEntity(PlatDTO dto) {
-        return null;
+        return modelMapper.map(dto, Plat.class);
     }
 }
